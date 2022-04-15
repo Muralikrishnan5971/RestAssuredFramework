6fa7366ba56423bf0856e08abc1e875a7cbede24
Muralikrishnan Ganesan <muralikrishnan.ganesan@sathaga.com>
2022-04-15 16:15:56 +0530

    rest assured framework set up

---
+++ src/test/java/com/mk/tests/GetTests.java
@@ -0,0 +1,14 @@
+package com.mk.tests;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class GetTests {
+
+	@Test
+	public void getEmployeeDetails() {
+		
+		
+	}
+}
