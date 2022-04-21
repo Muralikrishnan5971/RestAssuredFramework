08e3710af8d21b5f48a690f0bb1102cfdc37e5f3
Muralikrishnan Ganesan <muralikrishnan.ganesan@sathaga.com>
2022-04-22 00:08:53 +0530

    basic auth test

---
+++ src/test/java/com/mk/tests/AuthorizationTest.java
@@ -0,0 +1,37 @@
+package com.mk.tests;
+
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+
+public class AuthorizationTest {
+
+	
+	@Test(enabled = false)
+	public void basicAuthTest(){
+		
+		Response response = given()
+//				.auth()
+//				.basic("postman", "password")
+				.header("Authorization", "Basic cG9zdG1hbjpwYXNzd29yZA==")	// both does the same, as did in postman
+				.log()
+				.all()
+				.get("https://postman-echo.com/basic-auth");
+		
+		response.prettyPrint();
+	}
+	
+	@Test
+	public void getAllWorkspaceInPostman(){
+		
+		Response response = given()
+		.header("X-Api-Key", "PMAK-62619ae76f0b931a8d63f0e3-a420a5377ac9df59ad157699bd5ed85503")
+		.log()
+		.all()
+		.get("https://api.getpostman.com/workspaces");
+		
+		response.prettyPrint();
+	}
+}
